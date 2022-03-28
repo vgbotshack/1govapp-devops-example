825079fd9b7c91e543c8cbc6f6783fd378452975
Lesedi Ramahobo <devlesedi@gmail.com>
2022-03-29 00:52:55 +0200

    Changes in DOP-11

--- sam-app/hello-world/app.js
+++ sam-app/hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'Changes for video demonstration',
+                message: 'create a demo video',
                 // location: ret.data.trim()
             })
         }
